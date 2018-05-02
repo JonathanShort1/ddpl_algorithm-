e2d0fbaa393dc571c3b9c258d177cd7e1329af2f
JonathanShort1 <jonathan.short@live.co.uk>
2018-05-02 23:36:43 +0100

    more comments

--- ddpl.py
+++ ddpl.py
@@ -44,8 +44,8 @@ def isNegate(c, a):
 # peform unit propagation on the set of conjuective clauses
 def unit_prop(clauses, a):
     if a is None: return []
-    clauses[:] = [cl for cl in clauses if not a in cl]
-    clauses[:] = [isNegate(cl, a) for cl in clauses]
+    clauses[:] = [cl for cl in clauses if not a in cl] # completely remove clauses with unit clause present
+    clauses[:] = [isNegate(cl, a) for cl in clauses] # remove the negation of the unit clause from the clause
     return clauses
 
 # perform the DDPL algorithm on the set on clauses in CNF
